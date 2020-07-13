9c35d69c870c5bbc21efca275232538151170c81
Nur If Alan Farizi <if.alan2@gmail.com>
2020-07-14 06:35:59 +0700

    Initial Commit

---
+++ SampleCollectionView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleCollectionView
+//
+//  Created by Nur If Alan Farizi on 14/07/20.
+//  Copyright © 2020 Nur If Alan Farizi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
